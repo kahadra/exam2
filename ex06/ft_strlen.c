410d3a78712973699c3e713ac2957a1acf116fbd
Chunwoong Park <chpark@c2r7s8.42seoul.kr>
2021-09-14 20:44:36 +0900

    3

--- ex06/ft_strlen.c
+++ ex06/ft_strlen.c
@@ -1,30 +1,25 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_putstr.c                                        :+:      :+:    :+:   */
+/*   ft_strlen.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: chpark <chpark@student.42seoul.kr>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2021/09/14 20:28:32 by chpark            #+#    #+#             */
-/*   Updated: 2021/09/14 20:40:19 by chpark           ###   ########.fr       */
+/*   Created: 2021/09/14 20:39:26 by chpark            #+#    #+#             */
+/*   Updated: 2021/09/14 20:44:23 by chpark           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <unistd.h>
 
-void	ft_putchar(char c)
-{
-	write (1, &c, 1);
-}
-
-void	ft_putstr(char *str)
+int	ft_strlen(char *str)
 {
 	int	i;
 
 	i = 0;
-	while (str[i] != '\0')
+	while (srt[i] != '\0')
 	{
-		ft_putchar(str[i]);
-		i++
+		i++;
 	}
+	return (i);
 }
